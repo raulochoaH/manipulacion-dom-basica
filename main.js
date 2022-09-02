1bdf6c404da51b11f7ef89ac3351d8589915133c
Raul Ochoa <raulochoa3005@gmail.com>
2022-09-02 13:12:34 -0430

    Agregar .gitignore y primeros archivos del proyecto

---
+++ main.js
@@ -0,0 +1,25 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+
+btnCalcular.addEventListener('click', sumarInputValues);
+
+
+function sumarInputValues() {
+  console.log("Hiciste click");
+  }
+
+
+// function sumarInputValues(event) {
+//   console.log({event});
+//   event.preventDefault();
+//   const sumaInputs = +input1.value + +input2.value;
+//   pResult.innerText = "Resultado: " + sumaInputs;
+//   }
+
+// Para convertir strings a num
+// console.log(+input1.value + +input2.value);
